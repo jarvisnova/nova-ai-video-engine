1c143f2556e93557f7bc2e598b7a5c98d5c66c17
jarvisnova <krishnapepe23@gmail.com>
2025-06-18 16:36:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Entry point for NOVA App
+function startJarvis() {
+  alert("JARVIS Mode is now Active. Let's begin!");
+  console.log("NOVA JARVIS running...");
+
+  // Optional: use core functions if JarvisCore.js is loaded
+  if (typeof greetUser === "function" && typeof processCommand === "function") {
+    console.log(greetUser("Krishna"));
+    console.log(processCommand("launch cinematic"));
+  }
+}
